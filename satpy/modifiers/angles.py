73479e5c4903d2a80e1170db603ae3bf790998b4
Matthew Scutter <matthew@skysight.io>
2022-12-29 10:32:41 +1000

    Increase rounding to maximize cache hits at small accuracy expense

--- satpy/modifiers/angles.py
+++ satpy/modifiers/angles.py
@@ -269,10 +269,11 @@ def _sanitize_observer_look_args(*args):
         if isinstance(arg, datetime):
             new_args.append(STATIC_EARTH_INERTIAL_DATETIME)
         elif isinstance(arg, (float, np.float64, np.float32)):
-            # Round floating point numbers to nearest tenth. Numpy types don't
-            # serialize into JSON which is needed for hashing, thus the casting
-            # to float here:
-            new_args.append(float(round(arg, 1)))
+            if arg > 10000000:
+                new_args.append(round(arg/1000)*1000)
+            # round small floating point numbers to nearest tenth
+            else:
+                new_args.append(float(round(arg, 1)))
         elif _is_chunk_tuple(arg) and _chunks_are_irregular(arg):
             new_chunks = _regular_chunks_from_irregular_chunks(arg)
             new_args.append(new_chunks)
